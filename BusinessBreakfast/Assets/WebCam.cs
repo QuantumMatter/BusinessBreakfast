8b2271984a6f18d579d4b74ef6801c00afbdf61c
QuantumMatter <kopala.david@gmail.com>
2016-10-02 21:37:41 -0600

    Created Testing Scene: Cardboard;Cube w/ WebCamTexture;Sphere

---
+++ BusinessBreakfast/Assets/WebCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        WebCamTexture webCamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
