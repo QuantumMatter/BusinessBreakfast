74c2c449925e0c8fbab12406709b3ed19b526a07
QuantumMatter <kopala.david@gmail.com>
2016-10-04 20:14:46 -0600

    Update:Fixed Camera Orientation?

--- BusinessBreakfast/Assets/WebCam.cs
+++ BusinessBreakfast/Assets/WebCam.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class WebCam : MonoBehaviour {
 
+    WebCamTexture webCamTexture;
+    Quaternion baseRotation;
 	// Use this for initialization
 	void Start () {
-        WebCamTexture webCamTexture = new WebCamTexture();
+        webCamTexture = new WebCamTexture();
         Renderer renderer = GetComponent<Renderer>();
         renderer.material.mainTexture = webCamTexture;
+        baseRotation = transform.rotation;
         webCamTexture.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //transform.rotation = baseRotation * Quaternion.AngleAxis(webCamTexture.videoRotationAngle, Vector3.up);
 	}
 }
