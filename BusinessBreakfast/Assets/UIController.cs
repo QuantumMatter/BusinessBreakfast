009adb83f1a3ad40a491691b5c75c5b7aad29300
ca1ebd <cdqt98@gmail.com>
2016-10-06 10:14:49 -0600

    Created a working, but ugly UI

---
+++ BusinessBreakfast/Assets/UIController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //NextClicked();
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        if(SceneManager.GetActiveScene().buildIndex == 0){
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void NextClicked() {
+        Debug.Log("Next button was pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PreviousClicked()
+    {
+        Debug.Log("Next button was pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    /*public void HomeClicked()
+    {
+        Debug.Log("Next button was pressed");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }*/
+
+
+}
